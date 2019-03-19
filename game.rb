76550952e6d06655f9eae5dc8c607bb0ecd481d2
Patrick O Connor <padraigoc@gmail.com>
2019-03-19 22:34:41 +0000

    Lives are now decrementing for one player, when they incorrectly answer a question

--- game.rb
+++ game.rb
@@ -5,38 +5,46 @@ require './player'
 class Game
 
     def initialize
+      createNewPlayers
+     # @p1.lives == 0
     end
 
+
     def start
-
-      #create new players
-      createNewPlayers
-
-      #point to player 1
-
-
-      #point to player 2 
- 
-
+      while @p1.lives > 0 # && @p2.lives > 0
+  
       #ask question
-      newQuestion
-      end
+      newQuestion(@p1)
 
-      def newQuestion
+      end
+    end
+
+
+
+
+      def newQuestion(player)
         question = Question.new
         puts question.question_text
         answer = gets.chomp
         right_answer = question.correct_answer? answer
-        puts right_answer
+
+         if right_answer == false
+          puts "That is incorrect!"
+          player.lives -= 1
+          puts "Number of lifes left: #{player.lives}"
+         else 
+          puts "That is correct!"
+         end
+
+        #if answer is false, remove a life
       end
 
       def createNewPlayers
-        p1 = Player.new '1'
-        p2 = Player.new '2'
-        puts "Welcome to the game player #{p1.name} and player #{p2.name}"
-        puts "Player #{p1.name} has #{p1.lives} lives and player #{p2.name} has #{p2.lives} lives"
+        @p1 = Player.new '1'
+        @p2 = Player.new '2'
+        puts "Welcome to the game player #{@p1.name} and player #{@p2.name}"
+        puts "Player #{@p1.name} has #{@p1.lives} lives and player #{@p2.name} has #{@p2.lives} lives"
       end
-
 
   end
 
