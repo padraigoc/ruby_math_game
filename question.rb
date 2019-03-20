73ec245d68ec7c97df804383c3e8a5e1c80eb898
Patrick O Connor <padraigoc@gmail.com>
2019-03-20 04:24:12 +0000

    updating final code

--- question.rb
+++ question.rb
@@ -1,8 +1,8 @@
 class Question
 
     def initialize
-      @number1 = random_number
-      @number2 = random_number
+      @number1 = rand(20)
+      @number2 = rand(20)
     end
    
     def question_text
@@ -13,22 +13,4 @@ class Question
       @number1 + @number2 == some_answer.to_i
     end
 
-    def random_number
-        return rand(20)
-   end
-
 end
-   
-# question = Question.new
-# puts question.question_text
-# # text_of_question = question.question_text
-# right_answer = question.correct_answer? 10
-# puts right_answer
-
-
-
-# question = Question.new
-# puts question.question_text
-# answer = gets.chomp
-# right_answer = question.correct_answer? answer
-# puts right_answer
